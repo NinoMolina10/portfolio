fc3a782ae2250193abf9272df6b174ef6176ce5c
NinoMolina10 <nino.molina10@gmail.com>
2024-03-27 23:23:50 +0100

    finjournée

--- chess/Tour.js
+++ chess/Tour.js
@@ -12,28 +12,39 @@ const scene = new THREE.Scene()
 
 const camera = new THREE.PerspectiveCamera(70, iw / ih)
       
-const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
       
-const texture = new THREE.TextureLoader().load('diamond.jpg')
-const material = new THREE.MeshPhongMaterial({ map:texture })
+// const material = new THREE.MeshPhongMaterial({ color:0xffffff })
       
-const mesh = new THREE.Mesh(geometry, material)
+// const mesh = new THREE.Mesh(geometry, material)
 
-const light = new THREE.PointLight(0xeeeeee)
+const light = new THREE.PointLight(0xeeeeee, 3)
       
 scene.add(light)      
-scene.add(mesh)
+// scene.add(mesh)
       
 camera.position.set(0, 0, 3)
 light.position.set(0, 0, 3)
       
 const renderer = new THREE.WebGLRenderer({ canvas })
-            
-loop()
+  
+// Création d'une instance du chargeur GLTFLoader
+let loader = new THREE.GLTFLoader();
+
+// Chargement du modèle 3D à partir du fichier GLTF
+loader.load('chemin/vers/Tour3D.gltf', function(gltf) {
+    // Ajout du modèle 3D à la scène
+    scene.add(gltf.scene);
+
+    // Rendu de la scène avec la caméra spécifiée (assurez-vous que "camera" est défini)
+    renderer.render(scene, camera);
+});
+
+//loop()
       
-function loop() {
-    requestAnimationFrame(loop)
-    mesh.rotation.x += 0.005
-    mesh.rotation.y += 0.01
-    renderer.render(scene, camera)
-}
+// function loop() {
+//     requestAnimationFrame(loop)
+//     mesh.rotation.x += 0.005
+//     mesh.rotation.y += 0.01
+//     renderer.render(scene, camera)
+// }
